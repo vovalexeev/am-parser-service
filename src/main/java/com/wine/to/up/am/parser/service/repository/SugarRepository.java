732cea5e375a532a9297005432cb74b6fe34670e
SSyrova <isaisa552@gmail.com>
2020-10-04 13:23:00 +0300

    Added missing repositories.

---
+++ src/main/java/com/wine/to/up/am/parser/service/repository/SugarRepository.java
@@ -0,0 +1,12 @@
+package com.wine.to.up.am.parser.service.repository;
+
+import com.wine.to.up.am.parser.service.domain.entity.Sugar;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author : skorz
+ * @since : 04.10.2020, вс
+ **/
+public interface SugarRepository extends CrudRepository<Sugar, Long> {
+
+}
